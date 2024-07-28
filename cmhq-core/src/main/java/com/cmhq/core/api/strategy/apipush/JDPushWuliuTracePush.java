dad1bb6bc7efc4133152f6289b6d3e1e37bafd6b
Jiyang.Zheng <1010267837@qq.com>
2024-07-28 18:13:49 +0800

    edit

--- cmhq-core/src/main/java/com/cmhq/core/api/strategy/apipush/JDPushWuliuTracePush.java
+++ cmhq-core/src/main/java/com/cmhq/core/api/strategy/apipush/JDPushWuliuTracePush.java
@@ -142,8 +142,7 @@ public class JDPushWuliuTracePush extends AbstartApiTracePush<JDPushTraceDto>{
 
     @Override
     protected String getIsErrorMsg(JDPushTraceDto param) {
-
-        return "";
+        return param.getCategoryName();
     }
 
     @Override
