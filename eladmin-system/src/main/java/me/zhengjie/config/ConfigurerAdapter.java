6f6b8273a20fdd81f4c8234ca49473e3ccda4ef6
Jiyang.Zheng <1010267837@qq.com>
2024-07-31 22:35:19 +0800

    edit

--- eladmin-system/src/main/java/me/zhengjie/config/ConfigurerAdapter.java
+++ eladmin-system/src/main/java/me/zhengjie/config/ConfigurerAdapter.java
@@ -51,14 +51,6 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
         this.properties = properties;
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 设置允许跨域的路径
-                .allowedOrigins("*") // 设置允许跨域请求的源
-                .allowedMethods("POST", "GET", "PUT", "DELETE") // 设置允许跨域请求的方法
-                .allowedHeaders("*"); // 设置允许跨域请求的头
-
-    }
 
     @Bean
     public CorsFilter corsFilter() {
