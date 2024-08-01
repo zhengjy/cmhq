ce06860e72b3a8324e59ce8f5ccc4a0c996ec44c
Jiyang.Zheng <1010267837@qq.com>
2024-08-01 09:45:13 +0800

    edit

--- cmhq-core/src/main/java/com/cmhq/core/config/ContextHolderFilter.java
+++ cmhq-core/src/main/java/com/cmhq/core/config/ContextHolderFilter.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -16,6 +17,12 @@ public class ContextHolderFilter  implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+
 //        if (SecurityUtils.isPlatformCompany()){
 //            ContextHolder.setPlatform("company");
 //        }else {
